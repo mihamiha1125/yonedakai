8e011976db8de9458f6b4d398a6ffc9c6425db51
mihamiha1125 <71978815+mihamiha1125@users.noreply.github.com>
2026-06-09 14:01:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 // ---- 画像（Base64）----
 // IMG.logo と IMG.teirei は元のHTMLの <script> 内にある Base64 文字列を貼り付けてください。
-// 以下は変数名のみ定義しています。実際の値は元コードの IMG オブジェクトからコピーしてください。
+// 以下は変数名のみ定義しています。実際の値は元コードの IMG オブジェクトからコピーしてください
 const IMG = {
   logo: "img/IMG_9578.PNG",
   teirei: "img/IMG_0226.jpg"
